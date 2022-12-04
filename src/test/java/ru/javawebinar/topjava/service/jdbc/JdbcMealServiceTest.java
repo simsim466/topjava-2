21ae228c9efe15183e061352b8cbed436458af3c
anton <a.simonov@bankffin.ru>
2022-12-04 10:15:19 +0300

    part 6.7 hibernate and spring cache

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.topjava.service.jdbc;
 
 import org.springframework.test.context.ActiveProfiles;
+
+import org.junit.Ignore;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
