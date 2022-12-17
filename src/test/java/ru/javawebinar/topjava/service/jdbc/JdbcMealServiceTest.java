4de02f871665d14c3f695492d0b1492be46eef04
anton <a.simonov@bankffin.ru>
2022-12-17 08:56:02 +0300

    part 7.9 jackson object mapper

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
@@ -2,7 +2,6 @@ package ru.javawebinar.topjava.service.jdbc;
 
 import org.springframework.test.context.ActiveProfiles;
 
-import org.junit.Ignore;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
