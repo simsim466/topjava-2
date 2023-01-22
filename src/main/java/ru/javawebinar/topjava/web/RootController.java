fd72b41eaa2495da8c1c0b8784e1ee543a3a9687
anton <a.simonov@bankffin.ru>
2023-01-22 19:55:30 +0300

    part 10.1 hw meal binding

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -18,9 +17,6 @@ import ru.javawebinar.topjava.service.MealService;
 public class RootController {
 
     private static final Logger log = LoggerFactory.getLogger(RootController.class);
-
-    @Autowired
-    private MealService mealService;
 
     @GetMapping("/")
     public String root() {
@@ -41,10 +37,8 @@ public class RootController {
     }
 
     @GetMapping("/meals")
-    public String getMeals(Model model) {
+    public String getMeals() {
         log.info("meals");
-        model.addAttribute("meals",
-                MealsUtil.getTos(mealService.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay()));
         return "meals";
     }
 
